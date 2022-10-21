753a7541c2c98915286eda46a3d05c862364acc6
James Carter <carterjamesmike@gmail.com>
2022-10-21 15:30:04 -0500

    Adding user input to API functionalitiy in progress

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,37 +1,50 @@
-var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=0cd8570e03d2182052245fb88b455b9c';
-var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=30.267197359964705&lon=-97.7348848064757&appid=0cd8570e03d2182052245fb88b455b9c&units=imperial';
-
-
-// fetch(geocodeUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     console.log(data[0].lat);
-//     console.log(data[0].lon);
-//     // for (var i = 0; i < data.length; i++) {
-//     //   console.log(data[i].lat);
-//     //   console.log(data[i].lon);
-//     // }
-//   });
+var submitBtn = document.getElementById("submit-button");
+var lat; 
+var lon;
+    var forcastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0cd8570e03d2182052245fb88b455b9c&units=imperial`;
 
 
 
-fetch(forcastURL)
+
+
+function getAPI () {
+let input = document.getElementById("city-search").value;
+console.log(input);
+var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=0cd8570e03d2182052245fb88b455b9c`;
+
+fetch(geocodeUrl)
   .then(function (response) {
-    console.log("Hello?");
     return response.json();
   })
   .then(function (data) {
-    console.log("Something");
-    console.log(data)
+       lat = data[0].lat;
+       lon = data[0].lon;
+    console.log(typeof "lat");
+    console.log(`This is the lat ${lat} and lon ${lon}`);
+
+    console.log(forcastURL);
+  });
+
+console.log(typeof "lat");
+
+   fetch(forcastURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
     // console.log(data.list[0].main.temp);
-    for (var i =0; i < 5; i++) {
-        console.log("Something else " + i);
+    for (var i =0; i < 6; i++) {
+        console.log(`Day ${i+1}`);
+        var temp = data.list[i].main.temp;
+        var wind = data.list[i].wind.speed;
+        var humidity = data.list[i].main.humidity;
+        console.log(`This is day ${i+1} temp: ${temp} and the wind: ${wind} and the humidity: ${humidity}`);
         console.log(data.list[i].main.temp);
         console.log(data.list[i].wind.speed);
         console.log(data.list[i].main.humidity);
     }
   });
 
+}
+
+submitBtn.addEventListener('click', getAPI)
