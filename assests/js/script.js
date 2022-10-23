586131607413d4b028725c126411f365f4d7aae0
James Carter <carterjamesmike@gmail.com>
2022-10-23 14:38:33 -0500

    Started adding comments and fixing undefined user input bug

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,15 +1,15 @@
+//Element variables and array
 var submitBtn = document.getElementById('submit-button');
 var inputSubmit = document.getElementById('city-form');
 var inputCity = document.getElementById('city-name');
 var searchHistory = document.querySelector('#search-history');
 var searchArr = [];
 
-
+//Takes user input and fetches lat and lon from geocoding api as well as call renderHistory fx and getWeatherData fx and displays user selected city on current weather card
 function getGeocode () {
     var userInput = document.getElementById('city-search').value;
     renderHistory(userInput)
     var clearInput = document.getElementById('city-search');
-    // console.log(userInput);
     var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=0cd8570e03d2182052245fb88b455b9c`;
     inputCity.textContent = userInput;
     clearInput.value = '';
@@ -18,40 +18,37 @@ function getGeocode () {
             return response.json();
         })
         .then(function (data) {
+            if (data.length === 0) {
+                console.log("No go, man");
+                inputCity.textContent = "City";
+                searchArr.splice(0);
+                console.log(searchArr);
+            } else { 
+            console.log(data);
             var lat = data[0].lat;
             var lon = data[0].lon;
-            // console.log(`This is the lat ${lat} and lon ${lon}`);
             getWeatherData(lat, lon);
+            }
         });
 }
 
+//Fetches weather data from user selected city and displays it
 function getWeatherData (lat, lon,) {
-    // console.log(`getWeatherData fx lat: ${lat} and lon: ${lon}`);
     var forcastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0cd8570e03d2182052245fb88b455b9c&units=imperial`;
-    // console.log(forcastURL);
 
     fetch(forcastURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data.list[0].main.temp);
             const displayDate = moment();
             for (var i =0; i < 6; i++) {
-                // console.log(data);
-                // console.log(`Day ${i+1}`);
                 var temp = data.list[i].main.temp;
                 var wind = data.list[i].wind.speed;
                 var humidity = data.list[i].main.humidity;
                 var icon = data.list[i].weather[0].icon;
-                // console.log(`This is day ${i+1} temp: ${temp} and the wind: ${wind} and the humidity: ${humidity}`);
-                // console.log(data.list[i].main.temp);
-                // console.log(data.list[i].wind.speed);
-                // console.log(data.list[i].main.humidity);
-                // console.log(icon);
-                // console.log(icon.toString());
+
                 var tempText = document.getElementById(`current-temp-${i}`);
-                // tempText.textContent = `Temp: ${temp}`;
                 tempText.textContent = `Temp: ${temp}`;
                 var windText = document.getElementById(`current-wind-${i}`);
                 windText.textContent = `Wind: ${wind}`;
@@ -59,53 +56,44 @@ function getWeatherData (lat, lon,) {
                 humidityText.textContent = `Humidity: ${humidity}`;
                 var futureDate = document.getElementById(`date-${i}`);
                 futureDate.textContent = displayDate.add(i/i, 'd').format('dddd MMM Do');
+
                 document.getElementById(`img-${i}`).innerHTML = '<img src=https://openweathermap.org/img/wn/' + icon + '@2x.png' + ' width=50, height=50>'; 
                 
             }
         });
-
 }
 
+//Creates 'buttons' from user input history
 function renderHistory(userInput) {
     searchHistory.innerHTML = "";
-    // console.log("Hello?");
-    // console.log(userInput);
     searchArr.unshift(userInput);
 
     for (var i = 0; i < searchArr.length; i++) {
         var recentSearch = searchArr[i];
         var li = document.createElement("li");
         var button = document.createElement("button");
+        
         button.setAttribute("id", searchArr[i])
-
-   
         li.setAttribute("data-index", i);
-      
         button.textContent = recentSearch;
         li.appendChild(button);
         searchHistory.appendChild(li);
-        console.log(searchArr);
       }
 }
 
-
+//Pulls id from user input history buttons and calls seperate fx to fetch lat and lon from geocoding api
 searchHistory.addEventListener("click", function(event) {
     var element = event.target;
   
-
     if (element.matches("button") === true) {
         var btnAttribute= element.getAttribute('id');
-        // console.log(btnAttribute);
         getSearchGeocode(btnAttribute);
-        
-
-
     }
 });
 
+//Fetches lat and lon through geocoding api specifically for the user input history buttons and calls getWeatherData fx
 function getSearchGeocode (btnAttribute) {
     inputCity.textContent = btnAttribute;
-
     var geocodeUrlHistory = `http://api.openweathermap.org/geo/1.0/direct?q=${btnAttribute}&limit=1&appid=0cd8570e03d2182052245fb88b455b9c`;
 
     fetch(geocodeUrlHistory)
@@ -115,33 +103,29 @@ function getSearchGeocode (btnAttribute) {
         .then(function (data) {
             var lat = data[0].lat;
             var lon = data[0].lon;
-            // console.log(`This is the lat ${lat} and lon ${lon}`);
+
             getWeatherData(lat, lon);
         });
 }
 
-// function test () {
-    
-//     console.log('success!');
-// }
-    
+//Event listeners for form submit and submit button
 submitBtn.addEventListener('click', getGeocode);
-// inputSubmit.addEventListener('submit', test);
 inputSubmit.addEventListener('submit', function (event) {
     event.preventDefault();
     getGeocode();
 });
-// submitBtn.addEventListener('click', test)
+
 
 
 //Needs
                 //Display weather data on page
                 //Search history
                 //Search history funcitonality
-    //Add eventListener to submit function
+                //Add eventListener to submit function
                 //Link momemnt for date functionality
     //Minor styling
                 //Icons for weather
+    //Error for undefined user input
 
 
 //Wants
